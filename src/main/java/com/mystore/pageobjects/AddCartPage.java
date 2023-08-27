6502286694f895f748f65b2a06f3bf89a1f4fd05
Kiran Narawade <kiran.narawade80@gmail.com>
2023-08-27 21:58:00 +0530

    Added a Cart Page

---
+++ src/main/java/com/mystore/pageobjects/AddCartPage.java
@@ -0,0 +1,10 @@
+package com.mystore.pageobjects;
+
+public class AddCartPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("My cart branch");
+	}
+
+}
