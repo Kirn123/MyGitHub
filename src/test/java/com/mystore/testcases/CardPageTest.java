22fab12574e178d92671285502579158d1f67cd1
Kiran Narawade <kiran.narawade80@gmail.com>
2023-08-27 22:17:21 +0530

    New File Commited

--- src/test/java/com/mystore/testcases/CardPageTest.java
+++ src/test/java/com/mystore/testcases/CardPageTest.java
@@ -3,6 +3,7 @@ package com.mystore.testcases;
 public class CardPageTest {
 	public static void main(String[] args) {
 		System.out.println("Card Test Page");
+		System.out.println("Card Page Feature");
 	}
 
 }
